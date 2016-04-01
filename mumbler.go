06e9475b79ade44b6c1706fb053ee50fc587e72e
lime <etyle902@gmail.com>
2016-04-02 01:10:53 +0200

    Uploaded wrong file

--- mumbler.go
+++ mumbler.go
@@ -1,11 +1,9 @@
-package main
+package mumbler
 
 import (
 	"crypto/tls"
-	"flag"
-	"fmt"
+	"errors"
 	"net"
-	"os"
 	"strconv"
 
 	"github.com/layeh/gumble/gumble"
@@ -13,108 +11,113 @@ import (
 	_ "github.com/layeh/gumble/opus"
 )
 
-var (
-	SERVER   = flag.String("server", "localhost:64738", "Murmur server address")
-	USERNAME = flag.String("user", "mumbler", "client username")
-	INSECURE = flag.Bool("insecure", false, "skip server certificate verification")
-	CERT     = flag.String("cert", "", "user certificate file (PEM)")
-	KEY      = flag.String("key", "", "certificate key")
-	CHANNEL  = flag.String("chan", "Root", "Channel to join on connect")
-	CREATE   = flag.Bool("create-chan", false, "Create channel if nonexistent. Only works if -chan is specified")
-	FILE     = flag.String("f", "", "mp3 file to play")
-)
+type Mumbler struct {
+	playlist []string
+	config   *gumble.Config
+	client   *gumble.Client
+}
 
-func main() {
-	// stolen from gumbleutils
-	flag.Parse()
+func New() *Mumbler {
+	config := gumble.NewConfig()
+	config.TLSConfig.InsecureSkipVerify = true
+	return &Mumbler{
+		config: config,
+	}
+}
 
-	host, port, err := net.SplitHostPort(*SERVER)
+func (m *Mumbler) Name(n string) {
+	m.config.Username = n
+}
+
+func (m *Mumbler) Password(n string) {
+	m.config.Password = n
+}
+
+func (m *Mumbler) MoveToChannel(name string, create bool) err {
+	target := m.client.Channels.Find(name)
+
+	if target == nil {
+		root := m.client.Channels.Find()
+		if !create {
+			return errors.New("Nonexistent channel " + name)
+		}
+		if (*root.Permission() & gumble.PermissionMakeTemporaryChannel) == 0 {
+			return errors.New("Permission error: Cannot create channels in root")
+		}
+
+		root.Add(name, true)
+
+		target := client.Channels.Find(name)
+		if target == nil {
+			return errors.New("Failed to create channel " + name)
+		}
+	}
+	client.Self.Move(target)
+	return nil
+
+}
+
+func (m *Mumbler) AddFile(file ...string) {
+	m.playlist = append(m.playlist, file...)
+}
+
+func (m *Mumbler) ClearFiles() {
+	m.playlist = []string{}
+}
+
+func (m *Mumbler) Certificate(file, keyfile string) error {
+	m.config.TLSConfig.InsecureSkipVerify = false
+
+	if keyfile == nil || keyfile == "" {
+		keyfile = file
+	}
+	cert, err := tls.LoadX509KeyPair(file, keyfile)
 	if err != nil {
-		host = *SERVER
+		return err
+	}
+
+	m.config.TLSConfig.Certificates = append(m.config.TLSConfig.Certificates, cert)
+	return nil
+}
+
+func (m *Mumbler) Play() err {
+	for _, file := range m.playlist {
+		source := gumbleffmpeg.SourceFile(file)
+		m.stream = gumbleffmpeg.New(client, source)
+
+		if err := m.stream.Play(); err != nil {
+			return err
+		}
+	}
+}
+
+func (m *Mumbler) Server(address string) {
+	host, port, err := net.SplitHostPort(address)
+	if err != nil {
+		host = address
 		port = strconv.Itoa(gumble.DefaultPort)
 	}
 
-	// client
-	config := gumble.NewConfig()
-	config.Username = *USERNAME
-	config.Address = net.JoinHostPort(host, port)
+	m.config.Address = net.JoinHostPort(host, port)
+}
 
-	loadCert(config)
+func (m *Mumbler) Connect() err {
 
-	client := gumble.NewClient(config)
+	loadCert(m.config)
 
-	//keepAlive := make(chan bool)
+	m.client = gumble.NewClient(m.config)
 
-	/*client.Attach(gumbleutil.Listener{
-		Disconnect: func(e *gumble.DisconnectEvent) {
-			keepAlive <- true
-		},
-	})
-	*/
-	if err := client.Connect(); err != nil {
-		fmt.Printf("%s: %s\n", os.Args[0], err)
-		os.Exit(1)
+	if err := m.client.Connect(); err != nil {
+		return err
 	}
-	defer client.Disconnect()
 
 	// wait until connected
-	for client.State() == gumble.StateConnecting {
+	for m.client.State() == gumble.StateConnecting {
 	}
-	moveToChannel(client)
 
-	source := gumbleffmpeg.SourceFile(*FILE)
-	stream := gumbleffmpeg.New(client, source)
-
-	if err := stream.Play(); err != nil {
-		fmt.Println("Play error:", err)
-		os.Exit(1)
-	}
-	stream.Wait()
-
-	// <-keepAlive
+	return nil
 }
 
-func loadCert(config *gumble.Config) {
-	if *INSECURE {
-		config.TLSConfig.InsecureSkipVerify = true
-	}
-	if *CERT != "" {
-		if *KEY == "" {
-			KEY = CERT
-		}
-		cert, err := tls.LoadX509KeyPair(*CERT, *KEY)
-		if err != nil {
-			fmt.Printf("Failed to read certificate: %v\n", err)
-			os.Exit(1)
-		}
-
-		config.TLSConfig.Certificates = append(config.TLSConfig.Certificates, cert)
-	}
-}
-
-func moveToChannel(client *gumble.Client) {
-	if *CHANNEL != "Root" {
-		connChan := client.Channels.Find(*CHANNEL)
-		if connChan == nil {
-			root := client.Channels.Find()
-			if (*root.Permission() & gumble.PermissionMakeTemporaryChannel) == 0 {
-				fmt.Println("Fatal: cannot create channels in Root")
-				os.Exit(1)
-			}
-
-			if !*CREATE {
-				fmt.Printf("Fatal: Channel \"%v\" does not exist!\n", *CHANNEL)
-				os.Exit(1)
-			}
-			root.Add(*CHANNEL, true)
-			connChan := client.Channels.Find(*CHANNEL)
-			if connChan == nil {
-				fmt.Printf("Fatal: Failed to create channel %v\n", *CHANNEL)
-				os.Exit(1)
-			}
-			fmt.Printf("Created \"%v\"\n", *CHANNEL)
-		}
-		client.Self.Move(connChan)
-		fmt.Printf("Moved to \"%v\"\n", *CHANNEL)
-	}
+func (m *Mumbler) Disconnect() err {
+	return m.client.Disconnect()
 }
