8ec669f67828977e649d4404b947522ccdc56696
LimEJET <etyle902@gmail.com>
2016-04-02 02:30:28 +0200

    Update client.go

--- example/client.go
+++ example/client.go
@@ -1,18 +1,40 @@
 package main
 
 import (
-	"os"
+        "flag"
+        "fmt"
+        "github.com/LimEJET/mumbler"
+        "os"
+)
 
-	"github.com/LimEJET/mumbler"
+var (
+        NAME    = flag.String("n", "mumbler", "name to join server with")
+        SERVER  = flag.String("s", "localhost", "server to join")
+        CHANNEL = flag.String("c", "Root", "channel to join")
+        CREATE  = flag.Bool("create", false, "Allow create channel")
+        AVCONV  = flag.Bool("avconv", false, "Use avconv instead of ffmpeg")
 )
 
 func main() {
-	m := mumbler.New()
+        flag.Parse()
+        m := mumbler.New()
+        m.Name(*NAME)
+        m.Server(*SERVER)
+        if *AVCONV {
+                m.Command("avconv")
+        }
+        m.AddFile(os.Args[1:]...)
+        err := m.Connect()
+        if err != nil {
+                fmt.Println(err)
+                return
+        }
+        m.MoveToChannel(*CHANNEL, *CREATE)
+        err = m.Play()
+        if err != nil {
+                fmt.Println(err)
+                return
+        }
 
-	m.Name("Mumbler")
-	m.Server("localhost")
-	m.AddTracks(os.Args[1:]...)
-	m.Connect()
-	m.Play()
-
+        fmt.Println(m.Disconnect())
 }
