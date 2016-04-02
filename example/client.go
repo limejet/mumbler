121b63f9da42219ba8ab94d058e6e69aa4ed407e
LimEJET <etyle902@gmail.com>
2016-04-02 02:33:30 +0200

    Update client.go

--- example/client.go
+++ example/client.go
@@ -1,40 +1,41 @@
 package main
 
 import (
-        "flag"
-        "fmt"
-        "github.com/LimEJET/mumbler"
-        "os"
+	"flag"
+	"fmt"
+	"os"
+	"github.com/LimEJET/mumbler"
 )
 
 var (
-        NAME    = flag.String("n", "mumbler", "name to join server with")
-        SERVER  = flag.String("s", "localhost", "server to join")
-        CHANNEL = flag.String("c", "Root", "channel to join")
-        CREATE  = flag.Bool("create", false, "Allow create channel")
-        AVCONV  = flag.Bool("avconv", false, "Use avconv instead of ffmpeg")
+	NAME    = flag.String("n", "mumbler", "name to join server with")
+	SERVER  = flag.String("s", "localhost", "server to join")
+	CHANNEL = flag.String("c", "Root", "channel to join")
+	CREATE  = flag.Bool("create", false, "Allow create channel")
+	AVCONV  = flag.Bool("avconv", false, "Use avconv instead of ffmpeg")
 )
 
 func main() {
-        flag.Parse()
-        m := mumbler.New()
-        m.Name(*NAME)
-        m.Server(*SERVER)
-        if *AVCONV {
-                m.Command("avconv")
-        }
-        m.AddFile(os.Args[1:]...)
-        err := m.Connect()
-        if err != nil {
-                fmt.Println(err)
-                return
-        }
-        m.MoveToChannel(*CHANNEL, *CREATE)
-        err = m.Play()
-        if err != nil {
-                fmt.Println(err)
-                return
-        }
+	flag.Parse()
+	m := mumbler.New()
+	m.Name(*NAME)
+	m.Server(*SERVER)
+	if *AVCONV {
+		m.Command("avconv")
+	}
+	
+	m.AddFile(os.Args[1:]...)
+	
+	if err: m.Connect(); err != nil {
+		fmt.Println(err)
+		return
+  }
+	
+	m.MoveToChannel(*CHANNEL, *CREATE)
+	if err := m.Play(); err != nil {
+		fmt.Println(err)
+		return
+	}
 
-        fmt.Println(m.Disconnect())
+	fmt.Println(m.Disconnect())
 }
